977219d2f998e88cacfd44952896f820266e3998
moocupv <mooc@upv.es>
2026-01-20 12:10:30 +0100

    Update evaluate-certacles-writing-c1-LTI-conf.py add os library

--- evaluate-certacles-writing-c1-LTI-conf.py
+++ evaluate-certacles-writing-c1-LTI-conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import os
 # =================================================================
 # 🚀 CARGADOR DE VARIABLES DE ENTORNO (.env) EN EL MISMO DIRECTORIO
 # =================================================================
